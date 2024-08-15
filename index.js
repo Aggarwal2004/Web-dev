e9aa27030a28da366625b73aa8b7087f6cc8b37b
Aggarwal2004 <gargashmit61@gmail.com>
2024-08-15 21:50:43 +0530

    this a dice challenge in which on refresh one wins

--- index.js
+++ index.js
@@ -1,60 +1,54 @@
+var randomnumber1=Math.random();
+randomnumber1=randomnumber1*6;
+randomnumber1=Math.floor(randomnumber1)+1;
 
-for(var i=0;i<document.querySelectorAll(".drum ").length;i++){
-    document.querySelectorAll(".drum ")[i].addEventListener("click",handleclick);
+var randomnumber2=Math.random();
+randomnumber2=randomnumber2*6;
+randomnumber2=Math.floor(randomnumber2)+1;
+
+if(randomnumber1==1){
+    document.querySelector(".img1").setAttribute("src","./images/dice1.png");
 }
-// document.querySelector(".w").addEventListener("click",handleclick);
-function handleclick(){
-    var x=this.innerHTML;
-    makesound(x);
-    buttonAnimation(x);
+else if(randomnumber1==2){
+    document.querySelector(".img1").setAttribute("src","./images/dice2.png");
 }
-
-document.addEventListener("keydown",function(event){
-    makesound(event.key);
-    buttonAnimation(event.key);
-})
-
-function  makesound(key){
-    switch(key){
-        case "w":
-            var n=new Audio('./sounds/tom-4.mp3');
-            n.play();
-        break;
-
-        case "a":
-            var n=new Audio('./sounds/kick-bass.mp3');
-            n.play();
-        break;
-        case "s":
-            var n=new Audio('./sounds/tom-1.mp3');
-            n.play();
-        break;
-        case "d":
-            var n=new Audio('./sounds/snare.mp3');
-            n.play();
-        break;
-        case "j":
-            var n=new Audio('./sounds/tom-2.mp3');
-            n.play();
-        break;
-        case "k":
-            var n=new Audio('./sounds/crash.mp3');
-            n.play();
-        break;
-        case "l":
-            var n=new Audio('./sounds/tom-3.mp3');
-            n.play();
-        break;
-    }
+else if(randomnumber1==3){
+    document.querySelector(".img1").setAttribute("src","./images/dice3.png");
 }
-
-function buttonAnimation(cuurentKey){
-    var active=document.querySelector("."+cuurentKey);
-    active.classList.add("pressed");
-    // active.classList.remove("pressed");
-    setTimeout(function(){
-        active.classList.remove("pressed");
-    },100);
+else if(randomnumber1==4){
+    document.querySelector(".img1").setAttribute("src","./images/dice4.png");
 }
+else if(randomnumber1==5){
+    document.querySelector(".img1").setAttribute("src","./images/dice5.png");
+}
+else{
+    document.querySelector(".img1").setAttribute("src","./images/dice6.png");
+}
+    
+if(randomnumber2==1){
+    document.querySelector(".img2").setAttribute("src","./images/dice1.png");
+}
+else if(randomnumber2==2){
+    document.querySelector(".img2").setAttribute("src","./images/dice2.png");
+}
+else if(randomnumber2==3){
+    document.querySelector(".img2").setAttribute("src","./images/dice3.png");
+}
+else if(randomnumber2==4){
+    document.querySelector(".img2").setAttribute("src","./images/dice4.png");
+}
+else if(randomnumber2==5){
+    document.querySelector(".img2").setAttribute("src","./images/dice5.png");
+}
+else{
+    document.querySelector(".img2").setAttribute("src","./images/dice6.png");
+}
+   
 
-
+if(randomnumber1>randomnumber2){
+    document.querySelector("h1").textContent="Player 1 Wins";
+}else if(randomnumber1<randomnumber2){
+    document.querySelector("h1").textContent="Player 2 Wins";
+}else{
+    document.querySelector("h1").textContent="It's a Draw";
+}
